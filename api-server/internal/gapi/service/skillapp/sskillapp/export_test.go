57c0cb4cef9b4e79099e6d34b455299741be680c
dicoder <dicoder@126.com>
2026-06-22 12:58:09 +0800

    auto(api-server): export_test.go @ 12:58:09

--- api-server/internal/gapi/service/skillapp/sskillapp/export_test.go
+++ api-server/internal/gapi/service/skillapp/sskillapp/export_test.go
@@ -2,9 +2,16 @@ package sskillapp
 
 import (
 	"ginp-api/internal/gapi/entity"
+
+	"gorm.io/gorm"
 )
 
 // WriteMarketSkillForTest 写一条 market_skill,供 *_test.go 端到端验证 CheckUpdates。
 func (s *Service) WriteMarketSkillForTest(m *entity.MarketSkill) {
 	s.marketSkillModel().Create(m)
+}
+
+// GetDBForTest 暴露 dbWrite 给 *_test.go 用(白盒 export),用于断言 audit_log。
+func (s *Service) GetDBForTest() *gorm.DB {
+	return s.dbWrite
 }
