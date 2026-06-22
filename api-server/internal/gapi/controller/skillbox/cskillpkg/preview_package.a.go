461f9284067cd66ff0a53941a1942f861ea063ca
dicoder <dicoder@126.com>
2026-06-22 12:56:00 +0800

    auto(api-server): preview_package.a.go @ 12:56:00

--- api-server/internal/gapi/controller/skillbox/cskillpkg/preview_package.a.go
+++ api-server/internal/gapi/controller/skillbox/cskillpkg/preview_package.a.go
@@ -33,7 +33,7 @@ func PreviewPackage(c *gin.Context) {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
-	svc := sskillpkg.New(func() (*sskill.Service, error) {
+	svc := sskillpkg.New(dbs.GetWriteDb(), dbs.GetReadDb(), func() (*sskill.Service, error) {
 		return sskill.New(dbs.GetWriteDb(), dbs.GetReadDb(), store), nil
 	})
 	mf, err := svc.ParseManifest(body)
