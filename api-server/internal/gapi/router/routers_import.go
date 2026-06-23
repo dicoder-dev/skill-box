99e82e66a95735a5f77e39a1427ef3f993f2f6eb
dicoder <dicoder@126.com>
2026-06-23 16:21:57 +0800

    auto(api-server): routers_import.go @ 16:21:57

--- api-server/internal/gapi/router/routers_import.go
+++ api-server/internal/gapi/router/routers_import.go
@@ -14,6 +14,7 @@ import (
 	_ "ginp-api/internal/gapi/controller/skillbox/cmarket"
 	_ "ginp-api/internal/gapi/controller/skillbox/cskillpkg"
 	_ "ginp-api/internal/gapi/controller/skillbox/caudit"
+	_ "ginp-api/internal/gapi/controller/skillbox/cdesktop"
 	//{{placeholder_router_import}}//
 	// 上面的占位符请不要动动，否则会导致生成工具无法自动替换
 	//Please do not move the placeholders above, otherwise it will cause the generation tool to fail to replace them automatically
